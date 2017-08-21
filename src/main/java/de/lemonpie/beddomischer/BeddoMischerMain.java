cd488a9d08e0b0bde8ccaf3369e538bbfc7896ef
tobias <thinkdifferent055@gmail.com>
2017-08-21 13:29:55 +0200

    Add settings class and loader, saver

--- src/main/java/de/lemonpie/beddomischer/BeddoMischerMain.java
+++ src/main/java/de/lemonpie/beddomischer/BeddoMischerMain.java
@@ -1,9 +1,28 @@
 package de.lemonpie.beddomischer;
 
+import de.lemonpie.beddomischer.settings.Settings;
+import de.lemonpie.beddomischer.settings.SettingsHandler;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static spark.Spark.port;
 
 public class BeddoMischerMain {
 	public static void main(String[] args) {
 		port(9999);
+
+		Path settingsPath = Paths.get("settings.properties");
+		try {
+			if (Files.notExists(settingsPath)) {
+				SettingsHandler.saver().defaultSettings(settingsPath);
+			}
+			Settings settings = SettingsHandler.loader().load(settingsPath);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
---
+++ src/main/scala/de/lemonpie/beddomischer/settings/Settings.scala
@@ -0,0 +1,15 @@
+package de.lemonpie.beddomischer.settings
+
+/**
+  * CookNow Server Settings class.
+  *
+  * @author tobias
+  */
+class Settings {
+
+	val playerCount = 7
+
+    var readerInterface = "127.0.0.1"
+    var controlInterface = "127.0.0.1"
+    var httpInterface = "127.0.0.1"
+}
