7e8c9a041ad5d7616e1ae1f24dd7364010ad49ed
tobias <thinkdifferent055@gmail.com>
2018-09-01 10:08:01 +0200

    Change default interface configuration

--- src/main/java/de/lemonpie/beddomischer/ServerSettings.java
+++ src/main/java/de/lemonpie/beddomischer/ServerSettings.java
@@ -5,22 +5,22 @@ import de.tobias.utils.settings.Key;
 public class ServerSettings
 {
 	@Key("director.interface")
-	public String directorInterface = "localhost";
+	public String directorInterface = "0.0.0.0";
 	@Key("director.port")
 	public int directorPort = 9996;
 
 	@Key("reader.interface")
-	public String readerInterface = "localhost";
+	public String readerInterface = "0.0.0.0";
 	@Key("reader.port")
 	public int readerPort = 9997;
 
 	@Key("control.interface")
-	public String controlInterface = "localhost";
+	public String controlInterface = "0.0.0.0";
 	@Key("control.port")
 	public int controlPort = 9998;
 
 	@Key("http.interface")
-	public String httpInterface = "localhost";
+	public String httpInterface = "0.0.0.0";
 	@Key("http.port")
 	public int httpPort = 9999;
 
